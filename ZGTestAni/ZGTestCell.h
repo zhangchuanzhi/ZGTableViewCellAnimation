18171ee77e4a11c5a763775f6967d53ae13457e8
zhangzhi0903 <18310127735@163.com>
2018-02-27 17:24:53 +0800

    给tableViewCell加载显示动画

---
+++ ZGTestAni/ZGTestCell.h
@@ -0,0 +1,13 @@
+//
+//  ZGTestCell.h
+//  ZGTestAni
+//
+//  Created by offcn_zcz32036 on 2018/2/27.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGTestCell : UITableViewCell
+
+@end
---
+++ ZGTestAni/ZGTestCell.m
@@ -0,0 +1,24 @@
+//
+//  ZGTestCell.m
+//  ZGTestAni
+//
+//  Created by offcn_zcz32036 on 2018/2/27.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import "ZGTestCell.h"
+
+@implementation ZGTestCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
